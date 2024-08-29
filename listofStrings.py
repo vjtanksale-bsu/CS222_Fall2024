e4467130a132292b959962e119b360d933ce24d7
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2024-08-29 11:42:30 -0400

    List of strings and more list functions

---
+++ listofStrings.py
@@ -0,0 +1,9 @@
+def main():
+    students = ["Alice", "Bob", "Carol", "Dave", "Eve"]
+    print(len(students))
+    for s in students:
+        for c in s:
+            print(c)
+
+
+main()
--- lists.py
+++ lists.py
@@ -1,7 +1,13 @@
 def main():
-    midterm = [90, 80, 100, 75, 82]
+    midterm = [90, 80, 100, 75, 82, 80]
     print(midterm[1])
     for m in midterm:
         print(m)
     print(len(midterm))
+    print(max(midterm))
+    midterm.append(93)
+    midterm.insert(2, 70)
+    print(midterm)
+    midterm.remove(80)
+    print(midterm)
 main()
