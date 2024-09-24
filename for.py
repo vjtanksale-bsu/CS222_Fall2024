a6bfe3e09e9bcc62dc1e4c49dd16907e5530b2b8
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2024-09-23 20:55:16 -0400

    Updates to loopss

--- for.py
+++ for.py
@@ -3,3 +3,6 @@ for counter in range(15, 5, -3):
 for counter in range(10):
     if counter % 2 == 1:
         print("Odd number")
+for counter in range(20):
+    if counter % 2 == 0:
+        print("Even number")
