e95b544a0dd7997c6107873c30ae569f1bec0d83
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2024-10-01 12:11:40 -0400

    TDD and web service

--- test_bankaccount.py
+++ test_bankaccount.py
@@ -3,15 +3,13 @@ from bankaccount import BankAccount
 class TestBankAccount(unittest.TestCase):
     def setUp(self):
         self.account = BankAccount()
-    def test_Deposit(self):
         self.account.deposit(100)
+    def test_Deposit(self):
         self.assertEqual(self.account.balance, 100)
     def test_Withdraw(self):
-        self.account.deposit(100)
         self.account.withdraw(50)
         self.assertEqual(self.account.balance, 50)
     def test_Overdraw(self):
-        self.account.deposit(100)
         with self.assertRaises(ValueError):
             self.account.withdraw(200)
 
