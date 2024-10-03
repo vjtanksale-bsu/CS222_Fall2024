b747ec745bc9f36a90003392db619ffa80f40caa
vjtanksale-bsu <141774588+vjtanksale-bsu@users.noreply.github.com>
2024-10-03 12:09:53 -0400

    Weather web service update

--- weather.py
+++ weather.py
@@ -3,7 +3,8 @@ import ssl
 from urllib.request import urlopen
 
 def main():
-    url = "https://api.weather.gov/alerts/active?area=TX"
+    state = input("Enter two-character state code: ")
+    url = "https://api.weather.gov/alerts/active?area=" + state
     context = ssl._create_unverified_context()
     response = urlopen(url, context=context)
     weatherData = json.loads(response.read())
